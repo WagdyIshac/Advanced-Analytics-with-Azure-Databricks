17f949528b4fcb43012ee026051c95a50f5b888e
Wagdy Ishac <wagdy@outlook.com>
2021-05-02 13:07:53 +0400

    Update: LICENSE README.md notebooks

---
+++ notebooks/Users/wagdy@outlook.com/introduction/hello_world.py
@@ -0,0 +1,17 @@
+# Databricks notebook source
+print('I love Azure Databricks')
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC println("I Love Azure Databricks")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT "I Love Azure Databricks" as message
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC This is my first notebook using the magic command
